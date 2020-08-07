a2e9872fdd4d5654f3a070cdeeec57e0542bc980
Reinazhard <muh.alfarozy@gmail.com>
2020-08-07 20:59:21 +0700

    Add New Banner
    Signed-off-by: Reinazhard <muh.alfarozy@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=kang Kernel
+kernel.string=SiLonT Kernel by @eve_enryu on Telegram
 do.devicecheck=1
 do.modules=0
 do.systemless=1
